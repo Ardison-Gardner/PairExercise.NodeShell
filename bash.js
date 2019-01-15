38317a0d041756cbe7b9bf6710705319e242bc7c
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2019-01-15 12:39:41 -0500

    Seperated PWD into new module.

--- bash.js
+++ bash.js
@@ -1,14 +1,14 @@
+//Modules
+const pwd = require('./pwd');
+
 //Output a prompt
 process.stdout.write('prompt > ');
 
 //The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-
   if (cmd === 'pwd') {
-    let dir = process.cwd();
-    process.stdout.write(`${dir}`);
-    process.stdout.write('\nprompt > ');
+    pwd();
   } else {
     process.stdout.write(`You typed ${cmd}`);
     process.stdout.write('\nprompt > ');
---
+++ pwd.js
@@ -0,0 +1,10 @@
+//Print working directory
+
+function pwd () {
+  let dir = process.cwd();
+  process.stdout.write(`${dir}`);
+  process.stdout.write('\nprompt > ');
+}
+
+//Module export
+module.exports = pwd;
