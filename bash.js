106330bd35b35a705d399e8f39dd962a137b836d
Matthew Berman <mjberman024@gmail.com>
2019-01-15 12:16:32 -0500

    Added pwd command

--- bash.js
+++ bash.js
@@ -2,8 +2,15 @@
 process.stdout.write('prompt > ');
 
 //The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', (data) => {
+process.stdin.on('data', data => {
   const cmd = data.toString().trim();
-  process.stdout.write(`You typed ${cmd}`);
-  process.stdout.write('\nprompt > ');
+
+  if (cmd === 'pwd') {
+    let dir = process.cwd();
+    process.stdout.write(`${dir}`);
+    process.stdout.write('\nprompt > ');
+  } else {
+    process.stdout.write(`You typed ${cmd}`);
+    process.stdout.write('\nprompt > ');
+  }
 });
